a4a7c83e3dbdc9ccae4edc8e9bb6b81270c23361
Chanon Roy <chanonroy@gmail.com>
2016-04-10 23:21:06 -0400

    date parser added

--- scripts.js
+++ scripts.js
@@ -1,3 +1,19 @@
+// item ID (haircut, shave, etc.)
+  var id = '57082d0d081ddb10401d717a';
+
+// GETTING THE DATE (month/day/year)
+
+  var d = '04/26/2016';                                 // month/day/year TODO: test if January works (00)
+
+  var m = [" Jan ", " Feb ", " Mar ", " Apr ", " May ",
+           " June ", " July ", " Aug ", " Sept ", " Oct ",
+           " Nov ", " Dec "];
+
+  var month = m[Number(d.substring(0,2)) - 1];          // take month (04) and create index for month array
+
+  var date = d.substring(3,5) + month + d.substring(6); // '24 Apr 2016'
+    // var date_url = date.replace(/%20/g, " ");        // becomes '24%20Apr%202016'
+
 $('#button').click(function () {
 
   var $orders = $('#orders');
@@ -7,9 +23,7 @@ $('#button').click(function () {
   $('#orders').empty();
   $('#title').empty();
 
-  // date
-  var d = '26 Apr 2016';
-  var date = d.replace(/%20/g, " "); // becomes '24%20Apr%202016'
+  // header and key variables included in secrets.js (.gitignore)
 
   $.ajax({
     type: 'GET',
@@ -20,7 +34,7 @@ $('#button').click(function () {
         $title.append("Sorry, nothing available today!");
       }
       else { // if there is something
-        $title.append('Showing Dates For: ');
+        $title.append('Availability for ' + d);
         $.each(orders, function(i, order) { // FOR loop - i is the index
           $orders.append('<li>' + order.From.substring(12) + '</li>');
         });
